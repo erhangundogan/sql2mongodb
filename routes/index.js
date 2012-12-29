f21bc4446c56a73a2b8c0684c77e01816f3fad04
erhangundogan <erhan@trposta.net>
2012-12-28 18:34:02 -0800

    procedure

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,9 @@ var Connection = require('tedious').Connection,
     config = {
       userName: '',
       password: '',
-      server: '80.93.210.76',
+      server: '80.93.210.73',
       options: {
-        database: 'YouTravelDB'
+        database: 'My_EG_Search_2011'
       }
     };
 
@@ -39,8 +39,9 @@ exports.addressPost = function(req, res) {
             } else {
               res.write(result);
               res.write("<table class='table table-striped table-bordered table-condensed table-hover'>");
+              /*
               var queryString = "select top 100 * from " + req.body.visit.address;
-              request = new Request(queryString, function(err, rowCount) {
+              var request = new Request(queryString, function(err, rowCount) {
                 if (err) {
                   console.log(err);
                   res.write("</table>");
@@ -70,6 +71,57 @@ exports.addressPost = function(req, res) {
               });
 
               connection.execSql(request);
+              */
+
+              var params = {};
+              params.OperatorID = 0;
+              params.HotelID = "";
+              params.CountryCode = "EG";
+              params.City = "Hurghada";
+              params.Area = "";
+              params.Categories = "";
+              params.Boards = "";
+              params.Rooms = "";
+              params.CheckInDate = "2013-03-01";
+              params.CheckOutDate = "2013-03-10";
+              params.Adult = 2;
+              params.Child = 0;
+              params.ChildAge1 = 0;
+              params.ChildAge2 = 0;
+              params.UserID = 112000;
+              params.FirmID = 65;
+              params.Position = "web_Agent";
+
+              request = new Request("exec Src_HotelSearchv5", function(err, rowCount) {
+                if (err) {
+                  console.log(err);
+                  res.write("</table>");
+                  res.redirect("back");
+                } else {
+                  console.log(rowCount + ' rows');
+                }
+              });
+
+              request.on('row', function(columns) {
+                jade.renderFile(
+                  path.join(__dirname, "..", "views", "row.jade"), {
+                    "columns": columns
+                  },
+                  function(err, result) {
+                    if (err) {
+                      console.error("[ERROR] JadeFormatter, %s", err.stack||err);
+                    } else {
+                      res.write(result);
+                    }
+                });
+              });
+
+              request.on('doneInProc', function(rowCount, more) {
+                res.write("</table>");
+                res.end();
+              });
+
+              connection.execute(request, params);
             }
         });
       }
